da93c3db30a178ea2389e3c24d778682c02e0746
SquiryAP <artemper2002@gmail.com>
2025-05-13 21:07:06 +0400

    ss

--- src/main/java/com/fingerprint/controller/LogController.java
+++ src/main/java/com/fingerprint/controller/LogController.java
@@ -10,12 +10,14 @@ import com.fingerprint.service.UserService;
 import org.springframework.security.core.Authentication;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
+@RequestMapping("/api/log")
 public class LogController {
 
     @Autowired
